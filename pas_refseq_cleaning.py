9b20250626f861985b476f8b463b1831cbca1473
swirlycheese <126715421+swirlycheese@users.noreply.github.com>
2024-11-01 11:52:20 +0000

    PAS code to clean the file.

--- pas_refseq_cleaning.py
+++ pas_refseq_cleaning.py
@@ -1,12 +1,12 @@
 import pandas as pd
-
+#read csv
 df = pd.read_csv('pas_refseq.csv', dtype={34:str, 35:str, 36:str})
-
+#get rid of scaffold or minor within the data
 df1 = df[df["assembly_level"] != "Scaffold"]
 df2 = df[df["release_type"] != "Minor"]
 
-
+#drop any duplicates
 df3 = df.drop_duplicates(subset=["organism_name"], keep = "last")
 
-
+#make the cleaned data in a new csv file
 df.to_csv("cleanedpas.csv")
